89b70f6e8eebceb927ecd7c7fedc139fd951fa7b
didimbote-joao <didimbote_joao@outlook.com>
2024-02-25 17:41:48 +0100

    add insert in home page

--- app/Http/Controllers/Teste.php
+++ app/Http/Controllers/Teste.php
@@ -16,13 +16,17 @@ class Teste extends Controller
         // ];
         // return view('home');
 
+        // Inserção de sócio 
+        DB::insert("INSERT INTO socios VALUES (0, ?, ?, NOW(), NOW())", ['Didi', '3333']);
+
+        // Leitura dos socios da Base de dados
         // $resultado = DB::select("SELECT COUNT(*) Total FROM socios");
         $resultado = DB::select("SELECT * FROM socios");
         // echo '<pre>';
         // print_r($resultado);
         foreach($resultado as $socio){
             echo '<p>' . $socio->nome . '</p>';
-            
+
         }
     }
 
